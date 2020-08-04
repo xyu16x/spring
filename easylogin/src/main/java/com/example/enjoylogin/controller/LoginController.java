b7c2d30639ae5dd85e258514ea43ad54aab9d5ae
xyu16x <xyu16x@gmail.com>
2020-08-04 22:59:31 +0900

    *

---
+++ easylogin/src/main/java/com/example/enjoylogin/controller/LoginController.java
@@ -0,0 +1,61 @@
+package com.example.enjoylogin.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.enjoylogin.model.dao.UserRepository;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	UserRepository userRepos;
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/login")
+	public String login(
+			@RequestParam("userName") String userName,
+			@RequestParam("password") String password,
+			Model m) {
+		
+		String message ="Welcome! ";
+		
+		List<User> users = userRepos.findByUserNameAndPassword(userName, password);
+		if(users.size() > 0) {
+			User user = users.get(0);
+			message += user.getFullName();
+		} else {
+			message += "guest";
+		}
+		
+		m.addAttribute("message", message);
+		
+		return "login";
+		
+	}
+	
+	
+	
+	@ResponseBody
+	
+	public String showUsers() {
+		List<User> users = userRepos.findAll();
+		
+		User user = users.get(0);
+		
+		String info = user.getUserName() + " " + user.getPassword();
+		
+		return info;
+	}
+
+}
